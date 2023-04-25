38aae958037b6b3a389b7377251ad8b85d74d1c2
Samara Silvia <samara.silvia@bsit.intra>
2023-04-25 11:42:45 -0300

    Install MUI CSS and Updated NavBar

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,39 @@
+import * as React from 'react';
+
 import { Link } from "react-router-dom"
-import {FaStar} from 'react-icons/Fa'
+import { FaStar } from 'react-icons/Fa'
 
 const imageURL = import.meta.env.VITE_IMG;
 
-export function MovieCard({movie, showLink = true}) {
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
+export function MovieCard({movie, showLink = true}) {
   return (
-      <div className="movie-card">
-        <img src={imageURL + movie.poster_path} alt={movie.title}/>
-        <h2>{movie.title}</h2>
-        <p>
-          <FaStar/> {movie.vote_average}
-        </p>
-        {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
-      </div>
-  )
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={imageURL + movie.poster_path}
+          alt={movie.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {movie.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            <FaStar/> {movie.vote_average}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,239 @@
-import { Link } from "react-router-dom"
-import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import React, { useEffect, useState } from 'react';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import AdbIcon from '@mui/icons-material/Adb';
+
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const pages = ['Home', 'Movies', 'About'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: 20,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export function NavBar() {
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const [user, setUser] = useState({avatar:''})
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/samarasilvia21')
+      const data = await response.json();
+      console.log("DATAS: ", data);
+      setUser({
+        avatar: data.avatar_url
+    });
+  }
+  fetchData();
+  },[]);
+
   return (
-      <nav id="navbar">
-        <h2>
-          <Link to="/">
-            <BiCameraMovie/> MoviesLib
-        </Link>
-        </h2>
-        <form>
-            <input type="text" placeholder="Busque um filme"/>
-            <button type="submit">
-                <BiSearchAlt2/>
-            </button>
-        </form>
-      </nav>
-  )
+    <AppBar position="static" sx = {{ background: "#23232e" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+          }}
+        >
+        <Avatar 
+          sx={{width: "10em", height:'8em'}}
+          alt="Logo" 
+          src='https://user-images.githubusercontent.com/100232025/234303778-8184859c-3635-4105-b0fb-98768e1abc84.png' 
+        />
+        </Typography>
+        
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+            }}
+          >
+            <Avatar 
+              sx={{width: "100%", marginRight: 13,paddingRight: 3}}
+              alt="Logo" 
+              src='https://user-images.githubusercontent.com/100232025/234303778-8184859c-3635-4105-b0fb-98768e1abc84.png' 
+            />
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+
+          <Box sx={{ flexGrow: 0 }}>
+
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Profile Github" src={user.avatar} />
+              </IconButton>
+            </Tooltip>
+
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
