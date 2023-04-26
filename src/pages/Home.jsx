2e10d8e23980466c948cf5bb5ce89f3b89287800
aluno <aluno0@unicap.br>
2023-04-26 18:21:45 -0300

    Updated Home added slink images for TMDB

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,72 +1,111 @@
-import * as React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import ProductHeroLayout from './LayoutHome';
 
-const images = [
-  'https://images6.alphacoders.com/673/673797.jpg',
-  'https://images6.alphacoders.com/311/311266.jpg',
-  'https://images7.alphacoders.com/671/671281.jpg',
-  'https://images5.alphacoders.com/744/744742.jpg',
-  'https://images8.alphacoders.com/669/669878.jpg',
-  'https://images6.alphacoders.com/714/714381.jpg',
-  /*'https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80&w=1400',
-  'https://plus.unsplash.com/premium_photo-1667538962342-2d9937f014d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80&w=1400',
-  'https://images.unsplash.com/photo-1471341971476-ae15ff5dd4ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80&w=1400'*/
-];
+const imageURL = import.meta.env.VITE_IMG_SLINK;
 
 export const Home = () => {
-  const [currentImage, setCurrentImage] = React.useState(0);
 
-  React.useEffect(() => {
+  const [movies, setMovies] = useState([]);
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentImage((currentImage + 1) % images.length);
+      setCurrentImage((currentImage + 1) % movies.length);
     }, 5000);
 
     return () => clearInterval(intervalId);
-  }, [currentImage]);
+  }, [currentImage, movies]);
 
-  const backgroundImageStyle = {
-    backgroundImage: `url(${images[currentImage]})`,
-    backgroundColor: '#7fc7d9',
-    backgroundPosition: 'center',
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(imageURL);
+      const data = await response.json();
+      const moviesWithTagline = await Promise.all(
+        data.results.map(async (movie) => {
+          const response = await fetch(
+            `https://api.themoviedb.org/3/movie/${movie.id}?api_key=916eca463064fe2e8f5bf956de8e6682&language=en-US`
+          );
+          const data = await response.json();
+          return {
+            ...movie,
+            tagline: data.tagline,
+          };
+        })
+      );
+      setMovies(moviesWithTagline);
+    };    
+    fetchMovies();
+  }, []);
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 600,
   };
 
-  return (
-      <ProductHeroLayout sxBackground={backgroundImageStyle}>
-        <Typography color="inherit" align="center" variant="h2" marked="center">
-          Movies Library
-        </Typography>
-        <Typography
-          color="inherit"
-          align="center"
-          variant="h5"
-          sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
-        >
-          "The first rule of Fight Club is: You do not talk about Fight Club."
-        </Typography>
-        <Button
-          sx={{ 
-            backgroundColor: "#653DB6", 
-            minWidth: 200, 
-            '&:hover': { 
-              backgroundColor: '#7b5eb6', 
-              color: "#ffff",
-              transition: 'all 0.3s ease-in-out',
-              transform: 'scale(1.05)'
-            }
-          }}
-          variant="contained"
-          size="large"
-          component="a"
-          href="/movies/"
-        >
-          Movies
-        </Button>
-        <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
-          Discover the experience
-        </Typography>
-      </ProductHeroLayout>
+  const HeroText = () => (
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        textAlign: 'center',
+        zIndex: 1,
+      }}
+    >
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+        Movies Library
+      </Typography>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h5"
+        sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
+      >
+        {movies[currentImage]?.tagline ? movies[currentImage].tagline : 'Seja bem-vindo a um mundo de diversão e entretenimento'}
+      </Typography>
+      <Button
+        sx={{
+          backgroundColor: '#653DB6',
+          minWidth: 200,
+          '&:hover': {
+            backgroundColor: '#7b5eb6',
+            color: '#ffff',
+            transition: 'all 0.3s ease-in-out',
+            transform: 'scale(1.05)',
+          },
+        }}
+        variant="contained"
+        size="large"
+        component="a"
+        href="/movies/"
+      >
+        Movies
+      </Button>
+      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+        Discover the experience
+      </Typography>
+    </div>
   );
-}
+
+  return (
+    <ProductHeroLayout sxBackground={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movies[currentImage]?.backdrop_path})` }}>
+      <Slider {...sliderSettings}>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+              alt={movie.title}
+              style={{maxWidth: "1px", height: "1px"}}
+            />
+          </div>
+        ))}
+      </Slider>
+      <HeroText />
+  </ProductHeroLayout>
+  );
+};
