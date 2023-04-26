9e201ec61d1add78691e2e0e445dba3cd7ea173c
Samara Silvia <samara.silvia@bsit.intra>
2023-04-26 10:23:11 -0300

    Impoted Font Style Montesserat

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { Outlet } from "react-router-dom"
-import { NavBar } from "./components/NavBar"
-import { Footer } from "./components/Footer"
-import './App.css'
+import { Outlet } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { Footer } from "./components/Footer";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import './App.css';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'sans-serif'
+    ].join(','),
+  },
+});
 
 function App() {
   return (
-   <div className='App'>
-      <NavBar/>
-      <Outlet/>
-      <Footer/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <NavBar/>
+        <Outlet/>
+        <Footer/>
+      </div>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
